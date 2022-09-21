84e72675f3d0b5c09f8e3a7850905401e8023c39
tanf1 <Furkan.Tan-non-empl@moodys.com>
2022-09-21 11:13:46 -0400

    HistoryTable Deserialization Updated

--- SharedLibrary/src/main/java/com/esgc/APIModels/HistoryTableCategory.java
+++ SharedLibrary/src/main/java/com/esgc/APIModels/HistoryTableCategory.java
@@ -1,10 +1,12 @@
 package com.esgc.APIModels;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonDeserialize(using = HistoryTableDataResolverForHistoryTableCategoryDeserializer.class)
 public class HistoryTableCategory {
     public String name;
     public List<HistoryTableData> data ;
