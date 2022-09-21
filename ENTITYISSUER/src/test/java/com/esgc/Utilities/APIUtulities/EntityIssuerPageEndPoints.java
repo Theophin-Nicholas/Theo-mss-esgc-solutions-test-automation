c576b3e67dff01da2db0b5e77f3168ee5925664c
TanF1 <tanf1@moodys.com>
2022-09-21 16:58:30 -0400

    Development for ESGCA-9695, ESGCA-9769, ESGCA-9776

---
+++ ENTITYISSUER/src/test/java/com/esgc/Utilities/APIUtulities/EntityIssuerPageEndPoints.java
@@ -0,0 +1,14 @@
+package com.esgc.Utilities.APIUtulities;
+
+/**
+ * This utility class created to store all Endpoints for API calls
+ */
+
+public class EntityIssuerPageEndPoints {
+
+    public static String POST_sourceDocuments = "api/issuer/source-documents";
+    public static String POST_SUMMARY = "api/issuer/summary";
+    public static String POST_HEADER = "api/issuer/header";
+    public static String POST_CONTROVERSIES = "api/issuer/dual-materiality/controversies";
+
+}
