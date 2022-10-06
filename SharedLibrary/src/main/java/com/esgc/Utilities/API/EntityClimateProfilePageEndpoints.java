8357263ebf85c1dbbef1a71a3224cb73c118ca1e
Tarun <tarun.gupta1-non-empl.@moodys.com>
2022-10-05 21:36:22 -0700

    ESGCA 10503 + 10513 + 10522

--- SharedLibrary/src/main/java/com/esgc/Utilities/API/EntityClimateProfilePageEndpoints.java
+++ SharedLibrary/src/main/java/com/esgc/Utilities/API/EntityClimateProfilePageEndpoints.java
@@ -12,5 +12,6 @@ public class EntityClimateProfilePageEndpoints {
     public static String POST_ESGClimateSummary = "/api/entity/{orbis_id}/corpesgdata/esgasmt/climate-summary";
     public static String POST_ExportSourceDocuments = "/api/entity/{entity_id}/corpesgdata/esgasmt/source-documents";
     public static String POST_EntityUnderlyingDataMetrics = "/api/entity/{orbis_id}/physicalrisk/physicalriskmgmt/entity-underlying-data-metrics";
+    public static String POST_Header = "api/issuer/header";
 
 }
