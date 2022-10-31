45c13a23f85cda049f501b57b1b70f92acf5ea38
Mehmet Gul <mehmet.gul-non-empl@moodys.com>
2022-10-31 16:33:35 -0400

    Climate fix for smoke suite.

--- CLIMATE/src/test/java/com/esgc/Tests/UI/PortfolioAnalysisPage/ResearchLineWidgets/ResearchLineNavigation.java
+++ CLIMATE/src/test/java/com/esgc/Tests/UI/PortfolioAnalysisPage/ResearchLineWidgets/ResearchLineNavigation.java
@@ -19,10 +19,10 @@ public class ResearchLineNavigation extends UITestBase {
         BrowserUtils.wait(5);
 
         List<String> actualResearchLines = researchLinePage.getAvailableResearchLines();
-        assertTestCase.assertEquals(actualResearchLines.size(), 7, "Existing Research Lines Validation ");
+        assertTestCase.assertEquals(actualResearchLines.size(), 6, "Existing Research Lines Validation ");//TODO MAKE IT 7 AFTER ESG ASSESSMENTS
         assertTestCase.assertEquals(actualResearchLines.get(0), "Physical Risk Hazards", "Default Research Line Validation");
         List<String> expectedResearchLines = Arrays.asList("Physical Risk Hazards", "Physical Risk Management",
-                "Temperature Alignment", "Carbon Footprint", "Green Share Assessment", "Brown Share Assessment","ESG Assessments");
+                "Temperature Alignment", "Carbon Footprint", "Green Share Assessment", "Brown Share Assessment"/*,"ESG Assessments"*/);//TODO DE-SCOPPED
         assertTestCase.assertEquals(actualResearchLines, expectedResearchLines, "Research Line dropdown list verification");
 
     }
