70cc25966cb1a341d141f23f2e41764b1e81bede
TanF1 <tanf1@moodys.com>
2022-10-31 12:06:08 -0400

    Automation Updates

--- SharedLibrary/src/main/java/com/esgc/Reporting/CustomAssertion.java
+++ SharedLibrary/src/main/java/com/esgc/Reporting/CustomAssertion.java
@@ -7,6 +7,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.UnreachableBrowserException;
 import org.testng.asserts.IAssert;
 import org.testng.asserts.SoftAssert;
 
@@ -225,17 +226,26 @@ public class CustomAssertion extends SoftAssert {
      * take a name of a test and returns a path to screenshot takes
      */
     public static String getScreenshot(String name) throws IOException {
-        // name the screenshot with the current date time to avoid duplicate name
-        String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-        // TakesScreenshot ---> interface from selenium which takes screenshots
-        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        // full path to the screenshot location
-        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
-        File finalDestination = new File(target);
-        // save the screenshot to the path given
-        FileUtils.copyFile(source, finalDestination);
-        return target;
+        try {
+            // name the screenshot with the current date time to avoid duplicate name
+            String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+            // TakesScreenshot ---> interface from selenium which takes screenshots
+//        WebDriver driver = new RemoteWebDriver( ... );
+//        driver           = new Augmenter().augment( driver );
+//        ( (TakesScreenshot)driver ).getScreenshotAs( ... );
+            TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
+            File source = ts.getScreenshotAs(OutputType.FILE);
+            // full path to the screenshot location
+            String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
+            File finalDestination = new File(target);
+            // save the screenshot to the path given
+            FileUtils.copyFile(source, finalDestination);
+            return target;
+        }catch (UnreachableBrowserException e){
+            System.out.println("BROWSER IS NOT OPENED");
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
