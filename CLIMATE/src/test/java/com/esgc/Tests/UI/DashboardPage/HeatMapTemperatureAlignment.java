9e419e11ae1433b8b04d0e353e2c82d658f1f3d9
tanf1 <Furkan.Tan-non-empl@moodys.com>
2022-12-15 10:36:16 -0500

    Maven Build Process & Jira Process improvements

--- CLIMATE/src/test/java/com/esgc/Tests/UI/DashboardPage/HeatMapTemperatureAlignment.java
+++ CLIMATE/src/test/java/com/esgc/Tests/UI/DashboardPage/HeatMapTemperatureAlignment.java
@@ -1,6 +1,5 @@
 package com.esgc.Tests.UI.DashboardPage;
 
-import com.esgc.Pages.Dashboard.HeatMap;
 import com.esgc.Pages.DashboardPage;
 import com.esgc.Pages.LoginPage;
 import com.esgc.Tests.TestBases.DashboardUITestBase;
