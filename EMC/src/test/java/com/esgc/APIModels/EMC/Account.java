709e62373010f74b8b1276ad04f471e31ffc446f
TanF1 <tanf1@moodys.com>
2022-12-21 10:27:06 -0500

    EMC Updates

--- EMC/src/test/java/com/esgc/APIModels/EMC/Account.java
+++ EMC/src/test/java/com/esgc/APIModels/EMC/Account.java
@@ -1,2 +1,18 @@
-package com.esgc.APIModels.EMC;public class Account {
+package com.esgc.APIModels.EMC;
+
+import lombok.Data;
+
+@Data
+public class Account {
+
+    private String id;
+    private String name;
+    private boolean status;
+    private String contractStart;
+    private String contractEnd;
+    private String createdAt;
+    private String modifiedAt;
+    private String createdBy;
+    private String modifiedBy;
+
 }
