948b7026b4c39fdcfe994f6dd409928ab3e77258
tanf1 <81993350+tanf1@users.noreply.github.com>
2023-04-07 10:49:14 -0400

    Update OnDemandEndpoints.java

--- ONDemandReporting/src/test/java/com/esgc/ONDEMAND/API/EndPoints/OnDemandEndpoints.java
+++ ONDemandReporting/src/test/java/com/esgc/ONDEMAND/API/EndPoints/OnDemandEndpoints.java
@@ -9,7 +9,4 @@ public class OnDemandEndpoints {
 
     public static String POST_ON_DEMAND_FILTER = "/api/portfolio/{portfolioId}/on-demand/filter";
     public static String GET_ON_DEMAND_STATUS = "/api/portfolio/{portfolioId}/on-demand/status";
-
-    public static String GET_ON_DEMAND_INFO = "/api/on-demand/info";
-
   }
