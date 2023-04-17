22be2f5c73055fe8a11a4b3f2455b82503cb0760
elouadgi <issam.elouadghiri-non-empl@moodys.com>
2023-04-17 16:31:35 -0400

    issam-esgca-14298 no conflicts so far.

---
+++ ONDemandReporting/src/test/java/com/esgc/ONDEMAND/TestDataProviders/EntityWithEsgDataOnlyDataProviders.java
@@ -0,0 +1,18 @@
+package com.esgc.ONDEMAND.TestDataProviders;
+
+
+import org.testng.annotations.DataProvider;
+
+
+public class EntityWithEsgDataOnlyDataProviders {
+    @DataProvider(name = "entityWithEsgDataOnly-DP")
+    public Object[][] getEntityMethod() {
+
+        return new Object[][]{{"C5 Eiendom AS"},
+                {"Entergy Utility Affiliates LLC"},
+                {"Solutia, Inc."},
+                {"Resolution Life Australasia Ltd."}
+        };
+
+    }
+}
