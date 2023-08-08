b701b017aec0cb1072f91e64ef1e3bedb9b71457
TanF1 <tanf1@moodys.com>
2023-08-08 12:33:08 -0400

    ESGT-2643 Test Case Mapping Updates

--- ONDemandReporting/src/test/java/com/esgc/ONDEMAND/UI/Tests/OnDemandEntitlementBundleTests.java
+++ ONDemandReporting/src/test/java/com/esgc/ONDEMAND/UI/Tests/OnDemandEntitlementBundleTests.java
@@ -14,6 +14,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static com.esgc.Utilities.Groups.*;
